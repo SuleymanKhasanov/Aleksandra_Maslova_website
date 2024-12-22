148138cc82015d69bf49459b5b3221aa7ae308f6
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-12-22 20:21:21 +0500

    Новая улучшенная версия первого экрана

---
+++ src/entities/puzzles/assets/index.ts
@@ -0,0 +1,84 @@
+import puzzlePiece_0 from './puzzlePiece/puzzle_piece-0.svg';
+import puzzlePiece_1 from './puzzlePiece/puzzle_piece-1.svg';
+import puzzlePiece_2 from './puzzlePiece/puzzle_piece-2.svg';
+import puzzlePiece_3 from './puzzlePiece/puzzle_piece-3.svg';
+import puzzlePiece_4 from './puzzlePiece/puzzle_piece-4.svg';
+import puzzlePiece_5 from './puzzlePiece/puzzle_piece-5.svg';
+import puzzlePiece_6 from './puzzlePiece/puzzle_piece-6.svg';
+import puzzlePiece_7 from './puzzlePiece/puzzle_piece-7.svg';
+import puzzlePiece_8 from './puzzlePiece/puzzle_piece-8.svg';
+import puzzlePiece_9 from './puzzlePiece/puzzle_piece-9.svg';
+import puzzlePiece_10 from './puzzlePiece/puzzle_piece-10.svg';
+import puzzlePiece_11 from './puzzlePiece/puzzle_piece-11.svg';
+import puzzlePiece_12 from './puzzlePiece/puzzle_piece-12.svg';
+import puzzlePiece_13 from './puzzlePiece/puzzle_piece-13.svg';
+import puzzlePiece_14 from './puzzlePiece/puzzle_piece-14.svg';
+import puzzlePiece_15 from './puzzlePiece/puzzle_piece-15.svg';
+import puzzlePiece_16 from './puzzlePiece/puzzle_piece-16.svg';
+import puzzlePiece_17 from './puzzlePiece/puzzle_piece-17.svg';
+import puzzlePiece_18 from './puzzlePiece/puzzle_piece-18.svg';
+import puzzlePiece_19 from './puzzlePiece/puzzle_piece-19.svg';
+import puzzlePiece_20 from './puzzlePiece/puzzle_piece-20.svg';
+import puzzlePiece_21 from './puzzlePiece/puzzle_piece-21.svg';
+import puzzlePiece_22 from './puzzlePiece/puzzle_piece-22.svg';
+import puzzlePiece_23 from './puzzlePiece/puzzle_piece-23.svg';
+import puzzlePiece_24 from './puzzlePiece/puzzle_piece-24.svg';
+import puzzlePiece_25 from './puzzlePiece/puzzle_piece-25.svg';
+import puzzlePiece_26 from './puzzlePiece/puzzle_piece-26.svg';
+import puzzlePiece_27 from './puzzlePiece/puzzle_piece-27.svg';
+import puzzlePiece_28 from './puzzlePiece/puzzle_piece-28.svg';
+import puzzlePiece_29 from './puzzlePiece/puzzle_piece-29.svg';
+import puzzlePiece_30 from './puzzlePiece/puzzle_piece-30.svg';
+import puzzlePiece_31 from './puzzlePiece/puzzle_piece-31.svg';
+import puzzlePiece_32 from './puzzlePiece/puzzle_piece-32.svg';
+import puzzlePiece_33 from './puzzlePiece/puzzle_piece-33.svg';
+import puzzlePiece_34 from './puzzlePiece/puzzle_piece-34.svg';
+import puzzlePiece_35 from './puzzlePiece/puzzle_piece-35.svg';
+import puzzlePiece_36 from './puzzlePiece/puzzle_piece-36.svg';
+import puzzlePiece_37 from './puzzlePiece/puzzle_piece-37.svg';
+import puzzlePiece_38 from './puzzlePiece/puzzle_piece-38.svg';
+import puzzlePiece_39 from './puzzlePiece/puzzle_piece-39.svg';
+
+// Экспортируем объект с ссылками на все SVG файлы
+export const puzzlePieces = {
+  puzzlePiece_0,
+  puzzlePiece_1,
+  puzzlePiece_2,
+  puzzlePiece_3,
+  puzzlePiece_4,
+  puzzlePiece_5,
+  puzzlePiece_6,
+  puzzlePiece_7,
+  puzzlePiece_8,
+  puzzlePiece_9,
+  puzzlePiece_10,
+  puzzlePiece_11,
+  puzzlePiece_12,
+  puzzlePiece_13,
+  puzzlePiece_14,
+  puzzlePiece_15,
+  puzzlePiece_16,
+  puzzlePiece_17,
+  puzzlePiece_18,
+  puzzlePiece_19,
+  puzzlePiece_20,
+  puzzlePiece_21,
+  puzzlePiece_22,
+  puzzlePiece_23,
+  puzzlePiece_24,
+  puzzlePiece_25,
+  puzzlePiece_26,
+  puzzlePiece_27,
+  puzzlePiece_28,
+  puzzlePiece_29,
+  puzzlePiece_30,
+  puzzlePiece_31,
+  puzzlePiece_32,
+  puzzlePiece_33,
+  puzzlePiece_34,
+  puzzlePiece_35,
+  puzzlePiece_36,
+  puzzlePiece_37,
+  puzzlePiece_38,
+  puzzlePiece_39,
+};
